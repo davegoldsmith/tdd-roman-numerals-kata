ad4ba5c36654b826f3d1e74f1df57897e8778572
Dave Goldsmith <davegoldsmith@hotmail.com>
2022-12-20 15:41:22 +0000

    Added comments etc

--- src/roman-numeral-converter.js
+++ src/roman-numeral-converter.js
@@ -1,3 +1,6 @@
+/**
+ * List of possible roman numerals and corresponding number values
+ */
 const romanNumeralList = {
   M: 1000,
   CM: 900,
@@ -14,15 +17,24 @@ const romanNumeralList = {
   I: 1,
 };
 
+/**
+ * Converts a number between 1 and 3000 into a roman numeral
+ * @param {number} number number to convert
+ * @returns roman numeral equivalent
+ */
 export const numberToNumeral = (number) => {
   let result = "";
   if (typeof number !== "number" || Number.isInteger(number) !== true) {
+    // we can only convert numbers
     throw new Error("Value passed in not a number");
   } else if (number >= 3000 || number <= 0) {
+    // can only convert numbers in the range 1 to 3000
     throw new Error("Number out of range (must be between 1 and 3000)");
   } else {
     for (let key in romanNumeralList) {
       const value = romanNumeralList[key];
+      // handles cases like MM, II where the number can be divided
+      // by the value a number of times
       for (; number >= value; number -= value) {
         result += key;
       }
